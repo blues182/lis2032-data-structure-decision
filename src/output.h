ac7e2a7217622ebea9693afd1e83f8cf287462b3
blues182 <157330542+blues182@users.noreply.github.com>
2025-11-23 17:49:33 -0600

    Add output.h with function declarations

--- src/output.h
+++ src/output.h
@@ -1,1 +1,13 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include "decision.h"
+#include "questions.h"
+
+void printRationale(DataStructure ds, const Answers *a);
+void printDiagram(DataStructure ds);
+void printPseudocode(DataStructure ds);
+void printCourseExample(DataStructure ds);
+
+#endif
 
