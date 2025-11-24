c3b55ec3f76740ee8cab82804bd9ccbd07c67613
blues182 <157330542+blues182@users.noreply.github.com>
2025-11-23 22:04:14 -0600

    Add module docstring and metadata variables

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
+"""
+Sistema Guiado de Selección de Estructuras de Datos
+Paquete modular para recomendación de estructuras de datos
+"""
 
+__version__ = "1.0.0"
+__author__ = "Curso de Estructuras de Datos"
